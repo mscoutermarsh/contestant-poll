aa7d10d75d26c8fd7025e886c4b7eed391383635
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2012-01-14 22:41:39 +0000

    submit vote and confirm

--- vote.rb
+++ vote.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+require 'json'
 
 # If you want the logs displayed you have to do this before the call to setup
 DataMapper::Logger.new($stdout, :debug)
@@ -16,13 +17,45 @@ class Vote
   property :contestant,  Integer
   property :ip,          String
   property :headers,     String 
-  property :created_at,  DateTime 
+  property :created_at,  DateTime
+  property :validvote,       Boolean
+  property :answer,      Integer 
 end
+
+helpers do
+  def validateVote(answer, question)
+    ((question[0].to_i + question[1].to_i) == answer)
+  end
+end
+
 
 DataMapper.finalize
 
-@hello = Vote.create(:contestant => 1, :created_at => Time.now)
 
-post '/vote/:contestant/:code' do
+
+get '/vote/:contestant/?' do
+  # ask the user some simple math
+  int1 = 1 + rand(8)
+  int2 = 1 + rand(3)
+  answer = int1 + int2
+
+  @newVote = Vote.create(:contestant => params[:contestant], :created_at => Time.now, :answer=>answer, :validvote=>false, :ip=>@env['REMOTE_ADDR'])
   
+  output = Hash.new
+  output['question'] = "What is #{int1}+#{int2}?"
+  output['id'] = "#{@newVote.id}"
+
+  content_type "text"
+  output.to_json
+end
+
+get '/confirm/:id/:answer/?' do
+  vote = Vote.filter(id => params[:id])
+
+  if vote.answer == params[:answer] then
+    vote.update(:validvote => true)
+    "Thanks for voting!"
+  else
+    "Oops, wrong answer."
+  end
 end
