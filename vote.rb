84678d0817593fc0ccd4af5beae9e0824725754e
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2012-01-15 20:47:33 +0000

    referrer security

--- vote.rb
+++ vote.rb
@@ -7,24 +7,30 @@ require 'json'
 DataMapper::Logger.new($stdout, :debug)
 
 
-# xeround database connection
+# database connection
 DataMapper.setup(:default, ENV['VOTECONN'])
 
 class Vote
   include DataMapper::Resource
 
   property :id,          Serial
-  property :contestant,  Integer
+  property :contestant,  Integer, :index=>true
   property :ip,          String
   property :headers,     String 
   property :created_at,  DateTime
-  property :validvote,       Boolean
+  property :validvote,   Boolean, :index=>true
   property :answer,      Integer 
 end
+
+@referrer = ENV['ALLOWEDREF']
 
 helpers do
   def validateVote(answer, question)
     ((question[0].to_i + question[1].to_i) == answer)
+  end
+
+  def checkReferrer()
+    request.referer.match(@referrer)
   end
 end
 
@@ -32,43 +38,53 @@ end
 DataMapper.finalize
 
 post '/vote/:contestant/?' do
-  # ask the user some simple math
-  int1 = 1 + rand(8)
-  int2 = 1 + rand(3)
-  answer = int1 + int2
+  if checkReferrer() then
+    # ask the user some simple math
+    int1 = 1 + rand(8)
+    int2 = 1 + rand(3)
+    answer = int1 + int2
 
-  # create new vote
-  @newVote = Vote.create(:contestant => params[:contestant], :created_at => Time.now, :answer=>answer, :validvote=>false, :ip=>@env['REMOTE_ADDR'])
-  
-  # return security question and ID.
-  output = Hash.new
-  output['question'] = "What is #{int1}+#{int2}?"
-  output['id'] = "#{@newVote.id}"
+    # create new vote
+    @newVote = Vote.create(:contestant => params[:contestant], :created_at => Time.now, :answer=>answer, :validvote=>false, :ip=>@env['REMOTE_ADDR'])
+    
+    # return security question and ID.
+    output = Hash.new
+    output['question'] = "What is #{int1}+#{int2}?"
+    output['id'] = "#{@newVote.id}"
 
-  content_type "json"
-  output.to_json
+    content_type "json"
+    output.to_json
+  else
+    status(403)
+    "no, no, no..."
+  end
 end
 
 post '/confirm/:id/:answer/?' do
-  output = Hash.new
+  if checkReferrer() then
+    output = Hash.new
 
-  #answer to security question. If correct... vote is valid!
-  vote_to_check = Vote.get(params[:id])
-  if vote_to_check then
-    if vote_to_check.answer.to_i == params[:answer].to_i then
-      vote_to_check.update(:validvote => true)
-      output['message'] = "Thanks for voting!"
+    #answer to security question. If correct... vote is valid!
+    vote_to_check = Vote.get(params[:id])
+    if vote_to_check then
+      if vote_to_check.answer.to_i == params[:answer].to_i then
+        vote_to_check.update(:validvote => true)
+        output['message'] = "Thanks for voting!"
+      else
+        status(400)
+        output['message'] = "Oops, wrong answer."
+      end
     else
       status(400)
-      output['message'] = "Oops, wrong answer."
+      output['message'] = "no, no, no..."
     end
-  else
-    status(400)
-    output['message'] = "no, no, no..."
-  end
 
-  content_type "json"
-  output.to_json
+    content_type "json"
+    output.to_json
+  else
+    status(403)
+    "no, no, no..."
+  end
 end
 
 # return number of votes
